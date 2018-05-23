f7f357fd2eac0e81032ec663483f0647a634d023
Kyle Roberts <wkyleroberts@Ws-MacBook-Pro.local>
2018-05-23 15:30:42 -0400

    Class 2
    Greetings for various people

---
+++ course2.py
@@ -0,0 +1,11 @@
+import sys
+
+friend = sys.stdin.readline()
+friend = friend[0:-1]
+
+if friend == "bobbert" or friend == "dunker" :
+    print "wiggle wiggle"
+elif friend == "McTingle" :
+    print "Greetings Good Sir! How is your Hat collection?"
+else:
+    print "Whos in here?! My mouth cant take it!"
